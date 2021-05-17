9889f346a898ebece680d83cbda66d7ff273b537
JoJunHwa <jjj5306@naver.com>
2021-05-17 23:49:37 +0900

    4th commit

--- graph-search.c
+++ graph-search.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /*최대 vertex의 수는 10개로 설정한다.*/
 #define MAX_Vertex 10
 
 /*Graph는 Adjacent List로 구현한다.*/
 /*Edge 구조체는 Edge를 나타낸다 */
 typedef struct Edge {
-    struct Edge* link;
+	struct Edge* link;
 } Edge;
 
 /*Vertex 구조체는 Vertex를 인접 리스트로 나타내며 data로 Vertex에 들어있는 data, link로 다음 Edge를 가리킨다.*/
 typedef struct Vertex {
 	int data;
-    struct Edge* link;
+	struct Edge* link;
 } Vertex;
+
+/*Vertexhead 구조체는 배열로 선언되어 index에 맞게 vertex 번호를 가리키게 한다. */
+typedef struct Vertexhead {
+	struct Vertex* link;
+}Vertexhead;
 
 
 /* 현재 Vertex가 얼마나 있는지는 currentvertex로 관리한다. */
-int currentvertex=0;
+int currentvertex = 0;
 
 /* for stack */
 #define MAX_STACK_SIZE		20
@@ -38,20 +42,22 @@ Vertex* deQueue();
 void enQueue(Vertex* aVertex);
 
 
-void InitializeGraph(Vertex** v ); //Graph 초기화 함수
-void FreeGraph(Vertex* v); //Vertex free 함수
+void InitializeGraph(Vertexhead* v); //Graph 초기화 함수
+void FreeGraph(Vertexhead* v); //Vertex free 함수
 
-void Insertvertex(Vertex* v,int key); //key값의 vertex를 삽입한다.
+void Insertvertex(Vertexhead* v); //vertex를 삽입한다.
 void Insertedge();
 
 
 int main()
 {
 	char command;
-	int key;
-   /*adjList를 통해서 Graph를 나타 낼 것이다.*/
-    Vertex* adjList= NULL;
-	printf("2020039071 JoJunHwa");
+	
+	/*adjList를 통해서 Graph를 나타 낼 것이다. Insert Vertex를 호출하면 headList가 해당 번호에 맞게 vertex를 활성화한다. */
+	Vertexhead headList[MAX_Vertex] ;
+
+	printf("2020039071 JoJunHwa\n");
+
 	do {
 		printf("\n\n");
 		printf("----------------------------------------------------------------\n");
@@ -68,27 +74,33 @@ int main()
 
 		switch (command) {
 		case 'z': case 'Z':
-			InitializeGraph(&adjList);
+			InitializeGraph(headList);
 			break;
 		case 'v': case 'V':
-			
+			/*currentvertex가 MAX_Vertex를 넘지 않아야 한다*/
+			if (currentvertex >= MAX_Vertex)
+			{
+				printf("Vertex 수가 최대입니다.\n");
+				break;
+			}
+			Insertvertex(headList);
 			break;
 		case 'e': case 'E':
-			
+
 			break;
 		case 'd': case 'D':
-			
+
 			break;
 
 		case 'b': case 'B':
-			
+
 			break;
 		case 'p': case 'P':
-			
+
 			break;
 
 		case 'q': case 'Q':
-			
+
 			break;
 
 		default:
@@ -97,76 +109,96 @@ int main()
 		}
 
 	} while (command != 'q' && command != 'Q');
-    FreeGraph(adjList);
+	FreeGraph(headList);
 	return 1;
 }
 
-void InitializeGraph(Vertex** v )
+void InitializeGraph(Vertexhead* v)
 {
-    /*그래프가 비어있지 않다면 먼저 free를 진행해준다.*/
-    if (*v != NULL)
-		FreeGraph(*v);
-    /*MAX_Vertex만큼의 SIZE를 할당해준다.*/
-    *v = (Vertex*)malloc(sizeof(Vertex)*MAX_Vertex);
-    
-    /*adjList의 link와 data도 초기화 해준다. */
-    for(int i = 0;i<MAX_Vertex;i++)
-        {
-			(*v)->link = NULL;
-			(*v)->data = 0;
-		}
-    currentvertex = 0;
-    
-    /*큐와 스택을 위한 초기화*/
-    top = -1;
+	/*그래프가 비어있지 않다면 먼저 free를 진행해준다. currentcertex 는 insert vertex가 일어날 때 마다 ++되므로 0이 아니면 그래프가 비지 않았을 때 뿐이다*/
+	if (currentvertex != 0)
+		FreeGraph(v);
+
+	/*headList의 link도 초기화 해준다. */
+	for (int i = 0; i < MAX_Vertex; i++)
+		v[i].link = NULL;
+	currentvertex = 0;
+
+	/*큐와 스택을 위한 초기화*/
+	top = -1;
 
 	front = rear = -1;
 }
 
-void FreeGraph(Vertex* v)
+void FreeGraph(Vertexhead* v)
 {
-    Vertex* temp, * next;
-    int i=0;
-    /*첫 번째 for문에서 adjList vertex를 차례로 가리키게 함*/
-    for(temp = &v[0]; i < MAX_Vertex; i++, temp = &v[i])
-     {
-         while(1) //첫 번째 if문에 해당되면 pass하기위해 while문을 사용
-         {
-         if(temp->link == NULL) //해당 vertex에 edge가 없다면
-            break; //바로 반복문을 탈출한다
-         else if(temp->link->link == NULL) //해당 vertex에 edge가 하나라면
-				free(temp->link); //하나의 edge를 free한다
-		 else if(temp->link->link->link == NULL) //해당 vertex에 edge가 두 개라면
-		  {
-			free(temp->link->link); //두 번째 vertex를 free한다
-			free(temp->link); //첫 번째 vertex를 free한다
-		  }
-		 else
-		  {
-			temp = temp->link;
-			next = temp->link;
-			//현재 temp는 첫 번째 edge를 next는 두 번째 edge를 가리키고 있다.
-			do
+	Vertexhead* head;
+	Edge* temp, * next;
+	int i = 0;
+	/*그래프가 비어있는 경우 그냥 종료*/
+	if (currentvertex == 0)
+		return;
+	/*첫 번째 for문에서 adjList vertex를 차례로 가리키게 함*/
+	for (head = &v[0]; i < MAX_Vertex; i++, head = &v[i])
+	{
+		while (1) //첫 번째 if문에 해당되면 pass하기위해 while문을 사용
+		{
+			if (head->link == NULL) //해당 head에 vertex가 없다면
+				break; //바로 반복문을 탈출한다
+			else if (head->link->link == NULL) //해당 head에 edge는 없고 vertex만 존재한다면
 			{
+				free(head->link); //하나의 edge를 free한다
+				head->link = NULL;
+			}
+			else if (head->link->link->link == NULL) //해당 vertex에 edge가 한 개라면
+			{
+				free(head->link->link); //두 번째 vertex를 free한다
+				free(head->link); //첫 번째 vertex를 free한다
+				head->link = NULL;
+			}
+			else
+			{
+				temp = head->link->link;
+				next = temp->link;
+				//현재 temp는 첫 번째 edge를 next는 두 번째 edge를 가리키고 있다.
+				do
+				{
+					free(temp);
+					temp = next;
+					next = next->link;
+				} while (next->link != NULL); //반복문 안에서 temp를 free하고 temp와 next는 끝까지 이동한다
+				//반복문을 나왔으므로 temp와 next만 free해주면 해당 vertex는 free가 끝난다
 				free(temp);
-				temp = next;
-				next = next->link;
-			}while(next->link != NULL); //반복문 안에서 temp를 free하고 temp와 next는 끝까지 이동한다
-			//반복문을 나왔으므로 temp와 next만 free해주면 해당 vertex는 free가 끝난다
-			free(temp);
-			free(next);
-		  }
-         }
-     }
-	 //adjList만 free 해 주면 된다
-	 for(int i=0;i<MAX_Vertex;i++)
-	  free(&v[i]);
+				free(next);
+				free(head->link);
+				head->link = NULL;
+			}
+		}
+	}
 }
 
-void Insertvertex(Vertex* v,int key)
+void Insertvertex(Vertexhead* v)
 {
-	/*currentvertex 번호에 key 값을 가진 vertex 삽입. */
-	v[currentvertex].data = key;
-	v[currentvertex].link = NULL; //삽입된 vertex에는 edge가 아직 없다.
+	int vertexnumber = 0;
+	printf("Vertex Number (0~9) : ");
+	scanf("%d", &vertexnumber);
+
+	/*오류 검사*/
+	if (vertexnumber >= 0 && vertexnumber <= 9) //vertexnumber가 0~9인지 체크
+	{
+		if (v[vertexnumber].link != NULL) //vertexnumber의 vertex가 존재하는지 체크
+		{
+			printf("이미 존재하는 vertex 입니다.\n");
+			return;
+		}
+	}
+	else
+	{
+		printf("유효하지 않은 입력입니다.\n");
+		return;
+	}
+	/*vertexhead의 vertexnumber의 link는 vertex를 가리킬 것이다 */
+	v[vertexnumber].link = (Vertex*)malloc(sizeof(Vertex));
+	v[vertexnumber].link->link = NULL;
 	currentvertex++;
 }
